b9b7298da7715c5d8a4e6fa1e257701f188af43a
cotchan <zhtcks@gmail.com>
2021-07-03 19:47:13 +0900

    init project

---
+++ src/main/java/com/yhcy/aqc/AqcApplication.java
@@ -0,0 +1,13 @@
+package com.yhcy.aqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhcy/aqc/AqcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhcy.aqc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
