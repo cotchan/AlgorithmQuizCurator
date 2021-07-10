eac0b49d27fa92389638c0affd2bf43d99981f78
YH <kevin95392004@gmail.com>
2021-07-10 12:43:41 +0900

    PasswordEncoder 사용을 위한 Spring Security 의존성 주입

---
+++ src/main/java/com/yhcy/aqc/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.yhcy.aqc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**").anyRequest();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //SpringSecurity import 시 기본으로 모든 요청에 대해 로그인을 요구하는 기능 일시 중단
+        //이 후 해당 기능을 이용하려면 이 코드는 삭제해야함
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().
+                authenticated().and().formLogin().disable();
+    }
+}
