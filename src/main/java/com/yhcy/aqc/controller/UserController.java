37ac2e31e02cf009803ab7e289ce41887bec5999
YH <kevin95392004@gmail.com>
2021-07-10 12:16:21 +0900

    user model 오타 수정

--- src/main/java/com/yhcy/aqc/controller/UserController.java
+++ src/main/java/com/yhcy/aqc/controller/UserController.java
@@ -43,6 +43,7 @@ public class UserController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return "success";
     }
 }
--- src/main/java/com/yhcy/aqc/model/user/User.java
+++ src/main/java/com/yhcy/aqc/model/user/User.java
@@ -22,7 +22,7 @@ public class User extends BaseTimeEntity {
     private String userId;
 
     @Column(nullable = false, name = "nickname")
-    private String nickName;
+    private String nickname;
 
     @OneToOne
     @JoinColumn(nullable = false, name = "verify_question_seq")
@@ -36,10 +36,10 @@ public class User extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public User(Integer seq, String userId, String nickName, VerifyQuestion verifyQuestion, String verifyAnswer, Role role) {
+    public User(Integer seq, String userId, String nickname, VerifyQuestion verifyQuestion, String verifyAnswer, Role role) {
         this.seq = seq;
         this.userId = userId;
-        this.nickName = nickName;
+        this.nickname = nickname;
         this.verifyQuestion = verifyQuestion;
         this.verifyAnswer = verifyAnswer;
         this.role = role;
