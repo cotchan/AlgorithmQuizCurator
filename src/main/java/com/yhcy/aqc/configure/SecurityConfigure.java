812ef545f279235090d9234cc1ab5fc2d589bb6a
cotchan <zhtcks@gmail.com>
2021-07-13 19:57:10 +0900

    SecurityConfigure.java에 주석 추가

--- src/main/java/com/yhcy/aqc/configure/SecurityConfigure.java
+++ src/main/java/com/yhcy/aqc/configure/SecurityConfigure.java
@@ -38,16 +38,27 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 
     private final EntryPointUnauthorizedHandler unauthorizedHandler;
 
+    /**
+     * JwtTokenConfigure.java 클래스 설정값을 이용하여 TokenFilter를 빈으로 등록
+     */
     @Bean
     public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
         return new JwtAuthenticationTokenFilter(jwtTokenConfigure.getHeader(), jwt);
     }
 
+    /**
+     * 아래 패턴을 가진 URL에는 접근할 수 없도록 설정
+     */
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/swagger-resources", "/webjars/**", "/static/**", "/templates/**", "/h2/**");
     }
 
+    /**
+     * AuthenticationManager가 가지고 있는 AuthenticationProvider 목록에
+     * JwtAuthenticationProvider를 추가합니다.
+     * 그러면 AuthenticationManager가 인증을 할 때 JwtAuthenticationProvider를 사용하여 인증을 합니다.
+     */
     @Autowired
     public void configureAuthentication(AuthenticationManagerBuilder builder, JwtAuthenticationProvider authenticationProvider) {
         // AuthenticationManager 는 AuthenticationProvider 목록을 지니고 있다.
@@ -55,17 +66,26 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
         builder.authenticationProvider(authenticationProvider);
     }
 
+    /**
+     * AuthenticationProvider 인터페이스를 구현한 구현체 빈으로 등록
+     */
     @Bean
     public JwtAuthenticationProvider jwtAuthenticationProvider(Jwt jwt, UserService userService) {
         return new JwtAuthenticationProvider(jwt, userService);
     }
 
+    /**
+     * AuthenticationManager 빈 등록
+     */
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * PasswordEncoder 빈으로 등록
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -74,16 +94,24 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     // 요청이 거절된 경우는 DecisionManager가 튕겨낸 것입니다.
     // 모든 voter가 승인을 해야하는 것으로 셋팅을 했는데 WebExpressionVoter가 승인을 하지 않은 것입니다.
     // WebExpressionVoter는 WebSecurityConfigure::configure에 있는 설정들을 판단하는 애
+
+    /**
+     * 인증 과정이 끝난 후 인가 과정에서 사용하는 AccessDecisionManager 빈으로 등록
+     * 인가: 인증에 성공한 사용자가 해당 자원(URL)에 대해 접근 권한 유무를 판단 (보통 ROLE_로 판단한다고 보면 됨)
+     */
     @Bean
     public AccessDecisionManager accessDecisionManager() {
         List<AccessDecisionVoter<?>> decisionVoters = new ArrayList<>();
 
         decisionVoters.add(new WebExpressionVoter());
-        // voter 목록에 connectionBasedVoter 를 추가함
-        // 모든 voter 승인해야 해야함
+        // AccessDecisionManager가 가지고 있는 모든 voter가 접근을 승인해야 해당 자원에 대한 인가를 해주는 정책 사용
         return new UnanimousBased(decisionVoters);
     }
 
+    /**
+     * SpringSecurity 적용에 따라 URL 요청이 들어왔을 때 처리 방법 및 보호되는 리소스를 정의
+     * 필터 체인에 jwtAuthenticationTokenFilter 등록
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
