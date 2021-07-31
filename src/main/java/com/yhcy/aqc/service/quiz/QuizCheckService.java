407f510de072f937e89a273372393da6860812f9
cotchan <zhtcks@gmail.com>
2021-07-31 20:00:38 +0900

    UserRepository 래핑 서비스 추가

--- src/main/java/com/yhcy/aqc/service/quiz/QuizCheckService.java
+++ src/main/java/com/yhcy/aqc/service/quiz/QuizCheckService.java
@@ -1,14 +1,11 @@
 package com.yhcy.aqc.service.quiz;
 
-import com.yhcy.aqc.error.NotFoundException;
 import com.yhcy.aqc.model.quiz.Quiz;
 import com.yhcy.aqc.model.quiz.QuizState;
 import com.yhcy.aqc.model.quiz.QuizStateType;
 import com.yhcy.aqc.model.user.User;
-import com.yhcy.aqc.repository.user.UserRepository;
+import com.yhcy.aqc.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,19 +18,17 @@ import static com.google.common.base.Preconditions.checkArgument;
 @RequiredArgsConstructor
 public class QuizCheckService {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    private final UserRepository userRepository;
     private final QuizService quizService;
     private final QuizLogService quizLogService;
     private final QuizStateService quizStateService;
     private final QuizStateTypeService quizStateTypeService;
+    private final UserService userService;
 
     @Transactional
     public List<QuizState> update(final int userSeq, final List<QuizStateForUpdate> updateRequests) {
         checkArgument(updateRequests != null, "updateRequests must be not null");
 
-        final User user = userRepository.findById(userSeq).orElseThrow(() -> new NotFoundException(User.class, userSeq));
+        final User user = userService.findById(userSeq);
 
         List<QuizState> results = new LinkedList<>();
 
