10ca9e47d3c84ca9ebf3e2d44c83ecdaa40f8c9b
cotchan <zhtcks@gmail.com>
2021-08-01 22:53:02 +0900

    health check api 수정

--- src/main/java/com/yhcy/aqc/service/HealthCheckService.java
+++ src/main/java/com/yhcy/aqc/service/HealthCheckService.java
@@ -4,16 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import static java.lang.Thread.sleep;
-
 @Service
 public class HealthCheckService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    public Long healthCheck() throws InterruptedException {
-        sleep(5000);
-        log.info("HealthCheckService::healthCheck {}", Thread.currentThread().getName());
+    public Long healthCheck() {
         return System.currentTimeMillis();
     }
 }
